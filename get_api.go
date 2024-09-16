85612a37904ea5d5427e8d49a4474c67995b87b2
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-09-16 12:54:22 -0700

    go fmt

--- get_api.go
+++ get_api.go
@@ -1,89 +1,89 @@
 package youtube_api
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "net/url"
-    "os"
-    "strings"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
 
 func VideoListFromVideosURL(vu *url.URL) (VideoListResponse, error) {
 
-    var videoList = VideoListResponse{}
+	var videoList = VideoListResponse{}
 
-    resp, err := http.Get(vu.String())
-    if err != nil {
-        return videoList, err
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get(vu.String())
+	if err != nil {
+		return videoList, err
+	}
+	defer resp.Body.Close()
 
-    err = json.NewDecoder(resp.Body).Decode(&videoList)
-    if err != nil {
-        return videoList, err
-    }
+	err = json.NewDecoder(resp.Body).Decode(&videoList)
+	if err != nil {
+		return videoList, err
+	}
 
-    return videoList, nil
+	return videoList, nil
 }
 
 func SearchListFromSearchURL(su *url.URL) (SearchListResponse, error) {
 
-    var searchList = SearchListResponse{}
-    // fmt.Printf("%s\n", su.String())
+	var searchList = SearchListResponse{}
+	// fmt.Printf("%s\n", su.String())
 
-    resp, err := http.Get(su.String())
-    if err != nil {
-        return searchList, err
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return searchList, err
-    }
+	resp, err := http.Get(su.String())
+	if err != nil {
+		return searchList, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return searchList, err
+	}
 
-    if resp.StatusCode != 200 {
-        return searchList, fmt.Errorf("youtube error response: %v", string(body))
-    }
+	if resp.StatusCode != 200 {
+		return searchList, fmt.Errorf("youtube error response: %v", string(body))
+	}
 
-    err = json.Unmarshal(body, &searchList)
-    if err != nil {
-        return searchList, err
-    }
+	err = json.Unmarshal(body, &searchList)
+	if err != nil {
+		return searchList, err
+	}
 
-    return searchList, nil
+	return searchList, nil
 }
 
 func SearchURL(nextPageToken string, uuid string) *url.URL {
 
-    u, _ := url.Parse("https://www.googleapis.com/youtube/v3/search")
+	u, _ := url.Parse("https://www.googleapis.com/youtube/v3/search")
 
-    v := url.Values{}
-    v.Set("key", os.Getenv("YT_API_KEY"))
-    v.Add("part", "snippet")
-    v.Add("type", "video")
-    v.Add("maxResults", "50")
-    v.Add("order", "date")
-    v.Add("channelId", uuid)
+	v := url.Values{}
+	v.Set("key", os.Getenv("YT_API_KEY"))
+	v.Add("part", "snippet")
+	v.Add("type", "video")
+	v.Add("maxResults", "50")
+	v.Add("order", "date")
+	v.Add("channelId", uuid)
 
-    if len(nextPageToken) != 0 {
-        v.Set("pageToken", nextPageToken)
-    }
+	if len(nextPageToken) != 0 {
+		v.Set("pageToken", nextPageToken)
+	}
 
-    u.RawQuery = v.Encode()
-    return u
+	u.RawQuery = v.Encode()
+	return u
 }
 
 func VideosURL(videoIDs []string) *url.URL {
 
-    u, _ := url.Parse("https://www.googleapis.com/youtube/v3/videos")
+	u, _ := url.Parse("https://www.googleapis.com/youtube/v3/videos")
 
-    v := url.Values{}
-    v.Set("key", os.Getenv("YT_API_KEY"))
-    v.Add("part", "snippet,statistics,status,contentDetails")
-    v.Add("id", strings.Join(videoIDs, ","))
+	v := url.Values{}
+	v.Set("key", os.Getenv("YT_API_KEY"))
+	v.Add("part", "snippet,statistics,status,contentDetails")
+	v.Add("id", strings.Join(videoIDs, ","))
 
-    u.RawQuery = v.Encode()
-    return u
+	u.RawQuery = v.Encode()
+	return u
 }
